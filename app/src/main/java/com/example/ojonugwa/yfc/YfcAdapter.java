f510e1a4db27127c9f544ea1404aaca400af2fe3
Ojonugwa <ojonugwajusticealikali@gmail.com>
2018-08-20 13:45:41 +0100

    Create member_list_item for viewHolder

--- app/src/main/java/com/example/ojonugwa/yfc/YfcAdapter.java
+++ app/src/main/java/com/example/ojonugwa/yfc/YfcAdapter.java
@@ -1,4 +1,16 @@
 package com.example.ojonugwa.yfc;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 public class YfcAdapter {
+
+
+    class YfcViewHolder extends RecyclerView.ViewHolder {
+
+        public YfcViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
 }
